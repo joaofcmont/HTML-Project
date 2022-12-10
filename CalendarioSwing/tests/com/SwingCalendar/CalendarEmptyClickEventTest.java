dd228fc3cb84923fa0955ad7ce0ca21d43e35f09
matheusiscte <mpclq@iscte-iul.pt>
2022-12-10 13:27:15 +0000

    novas updates

---
+++ CalendarioSwing/tests/com/SwingCalendar/CalendarEmptyClickEventTest.java
@@ -0,0 +1,26 @@
+package com.SwingCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalendarEmptyClickEventTest {
+	
+CalendarEmptyClickEvent c;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		 c = new CalendarEmptyClickEvent(Object.class,(LocalDateTime.of(LocalDate.of(2022, 12, 10),LocalTime.of(12, 00))));
+	}
+	
+	@Test
+	void testGetDateTime() {
+		assertEquals(LocalDateTime.of(LocalDate.of(2022, 12, 10),LocalTime.of(12, 00)), c.getDateTime());
+	}
+
+}
