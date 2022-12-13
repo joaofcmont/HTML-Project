3f29ae25bfda95fe6b846f949d304f006b8e56f4
joaoiscte <jfcmo1@iscte-iul.pt>
2022-12-13 16:13:24 +0000

    Atualização propriedades

--- CalendarioSwing/tests/com/SwingCalendar/CalendarTest.java
+++ CalendarioSwing/tests/com/SwingCalendar/CalendarTest.java
@@ -1,8 +1,13 @@
 package com.SwingCalendar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,12 +38,15 @@ class CalendarTest {
 		assertEquals(LocalTime.of(12, 00),c.roundTime(t, m));
 		
 	}
+	
+	Date startDate= new GregorianCalendar(2022,8,12).getTime();
+	Date endDate=  new GregorianCalendar(2022,8,12).getTime();
 
 	@Test
-	void testDateInRange() {
-		fail("Not yet implemented");
-	}
-
+	void testDateInRange(Date testDate ) {
+		System.out.println(startDate);
+			assertTrue(testDate.after(startDate) && testDate.before(endDate));
+		}
 	@Test
 	void testGetDateFromDay() {
 		fail("Not yet implemented");
